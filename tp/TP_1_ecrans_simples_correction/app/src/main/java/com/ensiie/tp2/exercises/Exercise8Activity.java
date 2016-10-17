01394c2c4049ad426483ee35d7018e2e31ad247a
esteams <braciglianoadrian@gmail.com>
2016-10-17 17:45:38 +0200

    Ajout de la correction du TP 1

---
+++ tp/TP_1_ecrans_simples_correction/app/src/main/java/com/ensiie/tp2/exercises/Exercise8Activity.java
@@ -0,0 +1,19 @@
+package com.ensiie.tp2.exercises;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ensiie.tp2.R;
+
+/**
+ * Created by Adrian on 11/09/16.
+ */
+public class Exercise8Activity extends Activity{
+    private static final String TAG = "com.ensiie.tp2.Exercice1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_08);
+        setTitle(getString(R.string.exercise_8));
+    }
+}
